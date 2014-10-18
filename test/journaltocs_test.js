2a22b92df6cf02cfeed3f469c56cb921b7df0f39
Richard Smith-Unna <richardsmith404@gmail.com>
2014-10-18 22:09:32 +0100

    findJournals and journalDetails

--- test/journaltocs_test.js
+++ test/journaltocs_test.js
@@ -13,70 +13,82 @@ describe("JournalTOCs", function() {
 
   });
 
-describe(".findJournals()", function() {
+  describe(".findJournals()", function() {
 
-  this.timeout(20000);
+    this.timeout(20000);
 
-  it("should return an event emmiter", function() {
+    it("should return an event emmiter", function() {
 
-  });
-
-  it("should fetch matching journals", function(done) {
-    var email = "richardsmith404@gmail.com";
-    var jt = new JournalTOCs(email);
-
-    // here we search for 'bioinformatics', and we expect the
-    // journal Bioinformatics (http://bioinformatics.oxfordjournals.org/)
-    // to be in the results.
-    var fetcher = jt.findJournals('bioinformatics');
-
-    fetcher.on('result', function(result) {
-      var bioinf_journals = result;
-      var bioinf_issn = "1460-2059";
-      var result_issns = bioinf_journals.map(function(x) { x['prism:issn'] });
-      result_issns.indexOf(bioinf_issn).should.be.above(-1);
-      done();
     });
-  });
 
-});
+    it("should fetch matching journals", function(done) {
+      var email = "richardsmith404@gmail.com";
+      var jt = new JournalTOCs(email);
 
-describe(".journalDetails()", function() {
+      // here we search for 'bioinformatics', and we expect the
+      // journal Bioinformatics (http://bioinformatics.oxfordjournals.org/)
+      // to be in the results.
+      var fetcher = jt.findJournals('bioinformatics');
 
-  it("should get details of a journal", function() {
-    var email = "richardsmith404@gmail.com";
-    var jt = new JournalTOCs(email);
-
-    // here we get the details of the journal Bioinformatics using its issn
-    // and expect the title to be correct.
-    var result = jt.journalDetails('1460-2059');
-    result.should.have.property('title', 'Bioinformatics');
-  });
-
-});
-
-describe(".journalArticles()", function() {
-
-  it("should get a list of articles from a journal", function() {
-
-  });
-
-});
-
-describe(".articles()", function() {
-
-  it("should find articles matching a query", function() {
+      fetcher.on('result', function(result) {
+        var bioinf_issn = "1460-2059";
+        var issns = result.map(function(x) {
+          if (x.hasOwnProperty('prism:eissn')) {
+            return x['prism:eissn']['#'];
+          } else if (x.hasOwnProperty('prism:issn')) {
+            return x['prism:issn']['#'];
+          } else {
+            return 'NA';
+          }
+        });
+        issns.indexOf(bioinf_issn).should.be.above(-1);
+        done();
+      });
+    });
 
   });
 
-});
+  describe(".journalDetails()", function() {
 
-describe(".user()", function() {
+    it("should get details of a journal", function(done) {
+      var email = "richardsmith404@gmail.com";
+      var jt = new JournalTOCs(email);
 
-  it("should get a list of journals followed by a user", function() {
+      // here we get the details of the journal Bioinformatics using its issn
+      // and expect the title to be correct.
+      var fetcher = jt.journalDetails('1460-2059');
+
+      fetcher.on('result', function(result) {
+        result[0].should.have.property('title', 'Bioinformatics');
+        done();
+      });
+
+    });
 
   });
 
-});
+  describe(".journalArticles()", function() {
+
+    it("should get a list of articles from a journal", function() {
+
+    });
+
+  });
+
+  describe(".articles()", function() {
+
+    it("should find articles matching a query", function() {
+
+    });
+
+  });
+
+  describe(".user()", function() {
+
+    it("should get a list of journals followed by a user", function() {
+
+    });
+
+  });
 
 });
